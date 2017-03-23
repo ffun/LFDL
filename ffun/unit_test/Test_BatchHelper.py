76d04a9bb74cdc31e8d0e9bef67a7d9fcb8981e4
fang.junpeng <tfzsll@126.com>
2017-03-23 16:57:02 +0800

    BatchHelper的get_batch方法支持循环拿batch,并新增get_data和print_data显示数据

--- ffun/unit_test/Test_BatchHelper.py
+++ ffun/unit_test/Test_BatchHelper.py
@@ -1,4 +1,5 @@
-
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
 '''
 实际运行该单元测试时，需要放到具体模块下
 '''
@@ -10,8 +11,12 @@ b = [1,2,3,4,5,6,7]
 bh = BatchHelper((a, b))
 
 bh.shuffle(5)
-
+'''
 Checker.seq_len_check(bh.get_batch(5)[0], 5)
-Checker.seq_len_check(bh.get_batch(5)[0], 2)
+Checker.seq_len_check(bh.get_batch(5)[0], 5)
 
 print 'Test:BatchHelper OK'
+'''
+bh.print_data()
+for i in range(5):
+    print bh.get_batch(5)
